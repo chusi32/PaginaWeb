40812f7822541bd78dae1e63116ec0fe94dd267f
chusi32 <chusims3224@gmail.com>
2016-05-09 00:07:53 +0200

    Desarrollo paginas implementacion de JQuery Validate
    implementacion jquery validate, desarrollo de páginas

--- chusialegreclick/public_html/js/chusialegreclick.js
+++ chusialegreclick/public_html/js/chusialegreclick.js
@@ -33,26 +33,48 @@ $(document).ready(function(){
         {
             case 'liConocenos':
               $('#imgPagina').attr('src', 'imagenes/slide_logo.jpg');
-              $('#tituloPagina').text('CONOCENOS');
+              $('#tituloPagina').text('---CONOCENOS---');
               $('#divPaginasHeader').show();
               $('#divConocenos').show();
               break;
             case 'liBodas':
-              $('#tituloPagina').text('BODAS');
+              $('#imgPagina').attr('src', 'imagenes/slide_bodas.jpg');
+              $('#tituloPagina').text('---BODAS---');
               $('#divPaginasHeader').show();
               $('#divBodas').show();
               break;
+            case 'liBautizos':
+              $('#imgPagina').attr('src', 'imagenes/slide_bautizos.jpg');
+              $('#tituloPagina').text('---BAUTIZOS---');
+              $('#divPaginasHeader').show();
+              $('#divBautizos').show();
+              break;
+            case 'liComuniones':
+              $('#imgPagina').attr('src', 'imagenes/slide_comuniones.jpg');
+              $('#tituloPagina').text('---COMUNIONES---');
+              $('#divPaginasHeader').show();
+              $('#divComuniones').show();
+              break;
+            case 'liPeques':
+              $('#imgPagina').attr('src', 'imagenes/slide_peques.jpg');
+              $('#tituloPagina').text('---PEQUES---');
+              $('#divPaginasHeader').show();
+              $('#divPeques').show();
+              break;
+            case 'liFamilia':
+              $('#imgPagina').attr('src', 'imagenes/slide_familias.jpg');
+              $('#tituloPagina').text('---FAMILIAS---');
+              $('#divPaginasHeader').show();
+              $('#divFamilias').show();
+              break;
             case 'liContacto':
-              $('#tituloPagina').text('CONTACTO');
+              $('#tituloPagina').text('---CONTACTO---');
               $('#divPaginasHeader').show();
               $('#divContacto').show();
               break;
+            
         }
     });
-    
-//    $('#prueba').click(function(){
-//        $('#imagemodal').modal('show');
-//    });
 
     /*Cambiar el tipo de panel para saber cual está recibiendo el foco del ratón*/
     $('.panelInicio').mouseover(function(){
@@ -64,5 +86,48 @@ $(document).ready(function(){
         $(this).addClass('panel-warning');
     });
     
+    
+    
+     /*Comprobación formulario*/
+    $("#formulario").validate({
+    rules: {
+      nombre: {
+        required: true,
+        lettersonly: true
+      },
+      telefono: {
+        required: true,
+        digits: true,
+        minlength: 9,
+	maxlength: 9
+      },
+      email: {
+        required: true,
+	email: true
+      },
+      asunto: {
+        required: true
+      },
+      mensaje: {
+        required: true,
+        maxlength: 500
+      }
+    },
+    messages: {
+        nombre: 'Debes introducir un nombre.',  
+        email: 'Introduce un email valido.',
+        telefono: 'Introduce un telefono para facilitar el contacto.',
+        asunto: 'Debes indicar el motivo de tu mensaje.',
+        mensaje: 'Debes introducir un mensaje.',
+    },
+    submitHandler: function() {
+      alert("Se ha enviado tu mensaje.");
+    }
+  });
+    
+    
+    
+    
+     
 });
 
